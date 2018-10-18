0e736d8ccfcd1668793e333cd185bf87c2e4b4aa
MARK GOLDSTEIN <markgoldstein@MARKs-MBP-4.fios-router.home>
2018-10-18 11:21:52 -0400

    Runs, doesn't crash, time to check math and add asserts

--- src/mcmc.py
+++ src/mcmc.py
@@ -36,16 +36,12 @@ class UncollapsedGibbsIBP(nn.Module):
         N = X.size()[0]
         D = X.size()[1]
         pi = np.pi
-        sig_n2 = self.sigma_n.pow(2)
-      
+        sig_n2 = self.sigma_n.pow(2)      
         one = torch.tensor([1.0])
         log_first_term = one.log() - (N*D/2.)*(2*pi*sig_n2).log()
-
         log_second_term = ((-1./(2*sig_n2)) * \
             torch.trace((X-Z@A).transpose(0,1)@(X-Z@A)))
-
         log_likelihood = log_first_term + log_second_term
-        
         return log_likelihood
 
 
@@ -57,8 +53,8 @@ class UncollapsedGibbsIBP(nn.Module):
         p(z_nk=1) = m / (N-1)
         p(z_nk=1|Z_-nk,A,X) propto p(z_nk=1)p(X|Z,A)
         '''
+        N,D = X.size()
         
-        N = X.size()[0]
         # Prior on Z[i][k]
         Z_k = Z[:,k]
         m = Z_k.sum() - Z_k[i]
@@ -68,9 +64,6 @@ class UncollapsedGibbsIBP(nn.Module):
         Z_if_0[i,k] = 0
         log_prior_if_0 = (1 - (m/(N-1))).log()
         log_likelihood_if_0 = self.log_likelihood_given_ZA(X,Z_if_0,A)
-        
-        print("log prior if 0",log_prior_if_0)
-        print("log likelihood",log_likelihood_if_0)
         log_score_if_0 = log_prior_if_0 + log_likelihood_if_0
         
         # If Z_nk were 1
@@ -78,15 +71,11 @@ class UncollapsedGibbsIBP(nn.Module):
         Z_if_1[i,k]=1
         log_prior_if_1 = (m/(N-1)).log()
         log_likelihood_if_1 = self.log_likelihood_given_ZA(X,Z_if_1,A)
-        print("log_prior if 1", log_prior_if_1)
-        print("log_likelihood",log_likelihood_if_1)
         log_score_if_1 = log_prior_if_1 + log_likelihood_if_1
+        
+        # Exp, Normalize, Sample
         log_scores = torch.cat((log_score_if_0,log_score_if_1),0)
-        print("log scores shape",log_scores.size())
-        log_scores = log_scores - log_scores.max()
-        scores = log_scores.exp()
-        probs = scores / scores.sum()
-        print("probs",probs)
+        probs = self.renormalize_log_probs(log_scores) 
         p_znk = Bern(probs[1])
         return p_znk.sample()
 
@@ -114,8 +103,7 @@ class UncollapsedGibbsIBP(nn.Module):
                 # alternative: torch.potrf(a).diag().prod()
                 w_numpy = w.numpy()
                 sign,log_det = np.linalg.slogdet(w_numpy)
-                log_det = torch.tensor([log_det])
-                
+                log_det = torch.tensor([log_det],dtype=torch.float32)
                 # Note this is in log space
                 first_term = j*D*(sig_n/sig_a).log() - ((D/2)*log_det)
                  
@@ -134,7 +122,7 @@ class UncollapsedGibbsIBP(nn.Module):
             Z[i][-1]=1
         probs = self.renormalize_log_probs(log_probs)
         poisson_probs = poisson_probs / poisson_probs.sum()
-        sample_probs = torch.multiply(probs,poisson_probs)
+        sample_probs = probs*poisson_probs
         sample_probs = sample_probs / sample_probs.sum()
         Z = Z[:,:-truncation]
         assert Z.size()[1] == K
@@ -150,7 +138,7 @@ class UncollapsedGibbsIBP(nn.Module):
             truncation=100
             k_new = self.k_new(X,Z,A,i,truncation)
             if k_new > 0:
-                Z = torch.cat((Z,torch.zeros(N,k_new)),0)
+                Z = torch.cat((Z,torch.zeros(N,k_new)),1)
                 for j in range(k_new):
                     Z[i][-(j+1)] = 1
                 A_new = self.A_new(X,k_new,Z,A)
@@ -163,16 +151,10 @@ class UncollapsedGibbsIBP(nn.Module):
         Cov = sigma_n^2 (Z^T Z + (sigma_n^2/sigma_A^2) I)^{-1}
         p(A|X,Z) = N(mu,cov)
         '''
-        
-        print("shape z",Z.size())
-        print("shape x",X.size())
-        
-        
         N,D = X.size()
         K = Z.size()[1]
         ZT = Z.transpose(0,1)
         ZTZ = ZT@Z
-        print("shape ztz", ZTZ.size())
         I = torch.eye(K)
         sig_n = self.sigma_n
         sig_a = self.sigma_a
@@ -242,8 +224,10 @@ class UncollapsedGibbsIBP(nn.Module):
         return self.left_order_form(Z)
 
     def trim_ZA(self,Z,A):
-        sums = Z.torch.sum(dim=0)
-        to_keep = torch.where(sums !=0)[0]
+        N,K = Z.size()
+        sums = Z.sum(dim=0)
+        to_keep = sums > 0
+        print("to keep",to_keep)
         return Z[:,to_keep],A[to_keep,:]
 
     def gibbs(self, X, iters):
@@ -253,6 +237,7 @@ class UncollapsedGibbsIBP(nn.Module):
         Z = self.init_Z(N)
         A = self.init_A(K,D) 
         for iteration in range(iters):
+            print('iter:',iteration)
             A = self.resample_A(X,Z)
             Z,A = self.resample_Z(X,Z,A)
             Z,A = self.trim_ZA(Z,A)
